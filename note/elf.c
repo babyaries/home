d393ccfdccddcf1c12fc6739ee6220d2d86a8871
babyaries <babyaries@126.com>
2012-12-12 16:58:15 +0800

    how process call exec ()

--- note/elf.c
+++ note/elf.c
@@ -3,4 +3,6 @@ load_elf_binary -> kernel_read
 struct linux_binprm {
 typedef struct elf32_hdr{
 flush_old_exec
-exec_mmap -> mmdrop
+exec_mmap -> mmdrop -> find_vma
+create_elf_tables
+sys_exec -> search_binary_handler -> load_elf_binary -> start_thread
